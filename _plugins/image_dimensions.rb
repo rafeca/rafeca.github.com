64fda449407d71b6a90f85dc16203d89d92e6a03
osuka <webmaster@gatillos.com>
2012-02-02 00:07:44 +0100

    Added image dimension calculation (only for Mac OS X), and lazy loading with some animation

--- _plugins/image_dimensions.rb
+++ _plugins/image_dimensions.rb
@@ -2,10 +2,19 @@ module Jekyll
   module ImageDimensionsFilter
     def image_dimensions(input)      
       # this will only work in mac os x!!!
-      h_s = %x[sips -g pixelHeight "#{input}"]
-      w_s = %x[sips -g pixelWidth "#{input}"]
+      name="#{input}".strip
+      return "" unless File.exists?(name)
+      h_s = %x[sips -g pixelHeight "#{name}"]
+      w_s = %x[sips -g pixelWidth "#{name}"]
       h = h_s[/.*: (.*)/, 1]
       w = w_s[/.*: (.*)/, 1]
+      begin
+        h_i = Integer(h)
+        w_i = Integer(w)
+      rescue Exception => e
+        puts "  - bad image, or tool could not calculate size"
+        return ""
+      end
       "width='#{w}px' height='#{h}px'"
     end
   end
